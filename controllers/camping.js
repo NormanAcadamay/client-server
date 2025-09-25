fbeb818ef843b89958beaa040dd7439498e80570
NormanAcadamay <korawit4zx@gmail.com>
2025-09-25 21:00:23 +0700

    Update2-Ep14-Mac

---
+++ controllers/camping.js
@@ -0,0 +1,55 @@
+exports.listCamping = (req, res) => {
+  try {
+    // code body
+    console.log("Hello controller");
+    console.log(roitai);
+    res.send("Hello controller");
+  } catch (error) {
+    console.log(error.massage);
+    res.status(500).json({massage:'Server error'})
+  }
+};
+
+exports.readCamping = (req , res) => {
+  // code body
+  try {
+    
+    res.send("Hello Read",roimai);
+  } catch (error) {
+    console.log(error.massage);
+    res.status(500).json({massage:'Server error'})
+  }
+};
+
+exports.createCamping = (req , res) => {
+    try {
+         // code body
+         res.send("Hello Create");
+    } catch (error) {
+        console.log(error.massage);
+        res.status(500).json({massage:'Server error'});
+    }
+ 
+}
+
+exports.updateCamping = (req , res) => {
+    try {
+         // code body
+         res.send("Hello Update");
+    } catch (error) {
+        console.log(error.massage);
+        res.status(500).json({massage:'Server error'});
+    }
+ 
+}
+
+exports.deleteCamping = (req , res) => {
+    try {
+         // code body
+         res.send("Hello Delete");
+    } catch (error) {
+        console.log(error.massage);
+        res.status(500).json({massage:'Server error'});
+    }
+ 
+}
--- routes/camping.js
+++ routes/camping.js
@@ -1,46 +1,33 @@
 const express = require("express");
 const router = express.Router();
 
+// Controllers
+const { listCamping, readCamping , createCamping , updateCamping ,deleteCamping} = require("../controllers/camping");
+
 // ENDPOINT http://localhost:5001/api/camping
 // @Method GET [List CAMPING]
 // @ACCESS PUBLIC
-router.get("/camping", (req, res) => {
-    // code body
-  res.send("Hello route");
-});
+router.get("/camping", listCamping);
 
-// ENDPOINT http://localhost:5001/api/camping/5 
+// ENDPOINT http://localhost:5001/api/camping/5
 // @Method GET [Read CAMPING]
 // @ACCESS PUBLIC
-router.get("/camping/:id", (req, res) => {
-    // code body
-  res.send("Hello route");
-});
+router.get("/camping/:id", readCamping);
 
 // ENDPOINT http://localhost:5001/api/camping
 // @Method POST [CREATE CAMPING]
 // @ACCESS PRIVATE
-router.post("/camping", (req, res) => {
-    const {title,price} = req.body;
-    console.log(title) ;
-    console.log(price) ;
-  res.send("Hello POST, camping");
-});
+router.post("/camping", createCamping);
 
 // ENDPOINT http://localhost:5001/api/camping
 // @Method PUT [EDIT CAMPING]
 // @ACCESS PRIVATE
-router.put("/camping/:id", (req, res) => {
-    console.log(req.params.id);
-  res.send("jukkru Edit PUT");
-});
+router.put("/camping/:id", updateCamping);
 
 // ENDPOINT http://localhost:5001/api/camping/1
 // @Method DELETE [delete camping]
 // @ACCESS PRIVATE
-router.delete("/camping/:id", (req, res) => {
-    res.send('hello delete camping')
-})
+router.delete("/camping/:id", deleteCamping);
 
 //  export router ออกไป
 module.exports = router;
