4b1b859dbd64a46de0ad6172a3b150de077af10b
NormanAcadamay <korawit4zx@gmail.com>
2025-09-27 21:38:02 +0700

    Update5_Ep19-20_Mac

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,23 +1,15 @@
 const renderError = require("../utils/randerError");
 
-
-
-exports.createProfile = (req, res,next) => {
-    try {
-        // code body
-        const {firstname, lastname,clerkid} = req.body
-        if(true){
-            return renderError(res, 400, "ไปกินกาแฟก่อนนะ")
-        }
-        
-  
-        console.log(firstname, lastname,clerkid)
-        console.log('Hello Create Profile')
-        res.json({ message: "Hello Create Profile" });
-    
-    } catch (error) {
-        console.log(error.message)
-        next(error)
-    }
-
-}
+exports.createProfile = (req, res, next) => {
+  try {
+    // code body
+    const { firstname, lastname } = req.body;
+    console.log(req.headers.autorization);
+    console.log(firstname, lastname);
+    console.log("Hello Create Profile");
+    res.json({ message: "Hello Create Profile" });
+  } catch (error) {
+    console.log(error.message);
+    next(error);
+  }
+};
